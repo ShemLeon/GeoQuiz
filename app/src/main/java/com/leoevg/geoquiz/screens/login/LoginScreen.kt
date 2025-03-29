633114730f846bf8028f69a8939e834b7fc35488
ShemLeon <shemiakinleonid@gmail.com>
2025-03-29 14:22:06 +0300

    navigation setup

--- app/src/main/java/com/leoevg/geoquiz/screens/login/LoginScreen.kt
+++ app/src/main/java/com/leoevg/geoquiz/screens/login/LoginScreen.kt
@@ -1,10 +1,45 @@
 package com.leoevg.geoquiz.screens.login
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.provider.FontsContractCompat.Columns
+import com.leoevg.geoquiz.R
 import com.leoevg.geoquiz.navigation.NavigationPaths
 
 @Composable
 fun LoginScreen(navigate: (NavigationPaths) -> Unit){
-    Text("Hello World!")
+
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.app_login_title),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(top = 70.dp)
+        )
+        Column (modifier = Modifier.fillMaxWidth()){
+
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LoginScreenPreview(){
+    LoginScreen {  }
 }
