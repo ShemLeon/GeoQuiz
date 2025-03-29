d8ad205601dae9badecce6d58a0ea641d55ed65f
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2025-03-29 22:04:04 +0300

    Added TextFields

--- app/src/main/java/com/leoevg/geoquiz/screens/login/LoginScreen.kt
+++ app/src/main/java/com/leoevg/geoquiz/screens/login/LoginScreen.kt
@@ -1,30 +1,46 @@
 package com.leoevg.geoquiz.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.provider.FontsContractCompat.Columns
 import com.leoevg.geoquiz.R
 import com.leoevg.geoquiz.navigation.NavigationPaths
+import com.leoevg.geoquiz.ui.theme.Bg
+import com.leoevg.geoquiz.ui.theme.BlueGrey
 
 @Composable
 fun LoginScreen(navigate: (NavigationPaths) -> Unit){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Column(modifier = Modifier
-        .fillMaxSize(),
+        .fillMaxSize()
+        .background(Bg),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = stringResource(R.string.app_login_title),
@@ -32,8 +48,40 @@ fun LoginScreen(navigate: (NavigationPaths) -> Unit){
             fontWeight = FontWeight.SemiBold,
             modifier = Modifier.padding(top = 70.dp)
         )
-        Column (modifier = Modifier.fillMaxWidth()){
-
+        Column (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                placeholder = { Text(stringResource(R.string.email)) },
+                shape = RoundedCornerShape(15.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color.Transparent,
+                    disabledContainerColor = BlueGrey,
+                    focusedContainerColor = BlueGrey,
+                    errorContainerColor = BlueGrey,
+                    unfocusedContainerColor = BlueGrey
+                )
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(stringResource(R.string.password)) },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .padding(top = 10.dp),
+                shape = RoundedCornerShape(15.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedBorderColor = Color.Transparent,
+                    disabledContainerColor = BlueGrey,
+                    focusedContainerColor = BlueGrey,
+                    errorContainerColor = BlueGrey,
+                    unfocusedContainerColor = BlueGrey
+                )
+            )
         }
     }
 }
