a64fe73e8cd9b2b0f847c49d7e8892d522479a1d
ShemLeon <shemiakinleonid@gmail.com>
2025-04-30 17:19:10 +0300

    Authentication

--- app/src/main/java/com/leoevg/geoquiz/screens/quiz/QuizScreenViewModel.kt
+++ app/src/main/java/com/leoevg/geoquiz/screens/quiz/QuizScreenViewModel.kt
@@ -1,11 +1,29 @@
 package com.leoevg.geoquiz.screens.quiz
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.leoevg.geoquiz.data.model.Quiz
+import com.leoevg.geoquiz.domain.repository.AuthRepository
+import com.leoevg.geoquiz.domain.repository.QuizRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class QuizScreenViewModel : ViewModel() {
+@HiltViewModel
+class QuizScreenViewModel @Inject constructor(
+    private val quizRepository: QuizRepository
+): ViewModel() {
+    // state вьюхи
+    var currentQuiz by mutableStateOf<Quiz?>(null)
+
 // реализация получения вопросов
-    fun loadQuestions(quizTypeGame: String): Quiz {
-        TODO()
+    fun loadQuiz(quizTypeGame: String) {
+        viewModelScope.launch(Dispatchers.IO){
+            currentQuiz = quizRepository.loadQuiz(quizTypeGame)
+        }
     }
 }
