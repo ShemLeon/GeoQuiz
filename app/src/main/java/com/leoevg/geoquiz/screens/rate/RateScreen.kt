e7e96f5dc2bea2b2d31416a98e7ed8071696799d
ShemLeon <shemiakinleonid@gmail.com>
2025-05-17 14:09:23 +0300

    Rate

--- app/src/main/java/com/leoevg/geoquiz/screens/rate/RateScreen.kt
+++ app/src/main/java/com/leoevg/geoquiz/screens/rate/RateScreen.kt
@@ -1,5 +1,6 @@
 package com.leoevg.geoquiz.screens.rate
 
+import android.R.attr.rating
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
