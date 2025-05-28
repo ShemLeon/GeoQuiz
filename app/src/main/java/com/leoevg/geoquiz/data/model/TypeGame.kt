8e380edaf429732d3f15845e7d60697ca0690a92
ShemLeon <shemiakinleonid@gmail.com>
2025-05-29 02:06:07 +0300

    shade pic

--- app/src/main/java/com/leoevg/geoquiz/data/model/TypeGame.kt
+++ app/src/main/java/com/leoevg/geoquiz/data/model/TypeGame.kt
@@ -12,12 +12,10 @@ data class TypeGame(
 )
 
 val typeGames = listOf(
-    TypeGame(1, R.string.easy, R.drawable.card_easy, R.string.easyDesc, 1.0),
-    TypeGame(2, R.string.hard, R.drawable.card_hard, R.string.hardDesc, 2.0),
-    TypeGame(3, R.string.nightmare, R.drawable.card_nigthmare,R.string.nightmareDesc,3.0),
-    TypeGame(4, R.string.clothing, R.drawable.card_clothing,R.string.clothingDesc, 2.0),
-    TypeGame(5, R.string.history, R.drawable.card_history,R.string.historyDesc, 2.0),
-    TypeGame(6, R.string.sport, R.drawable.card_sport,R.string.sportDesc, 2.0)
+    TypeGame(1, R.string.easy,      R.drawable.card_easy,       R.string.easyDesc,      1.0),
+    TypeGame(2, R.string.hard,      R.drawable.card_hard,       R.string.hardDesc,      2.0),
+    TypeGame(3, R.string.nightmare, R.drawable.card_nigthmare,  R.string.nightmareDesc, 3.0),
+    TypeGame(4, R.string.clothing,  R.drawable.card_clothing,   R.string.clothingDesc,  2.0),
+    TypeGame(5, R.string.history,   R.drawable.card_history,    R.string.historyDesc,   2.0),
+    TypeGame(6, R.string.sport,     R.drawable.card_sport,      R.string.sportDesc,     2.0)
 )
-
-// описание в файле read.md
