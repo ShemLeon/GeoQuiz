25821a3656ccfe2aec85e58c3239314023c1a2de
ShemLeon <shemiakinleonid@gmail.com>
2025-05-29 02:11:53 +0300

    shade pic

--- app/src/main/java/com/leoevg/geoquiz/data/model/TypeGame.kt
+++ app/src/main/java/com/leoevg/geoquiz/data/model/TypeGame.kt
@@ -16,6 +16,6 @@ val typeGames = listOf(
     TypeGame(2, R.string.hard,      R.drawable.card_hard,       R.string.hardDesc,      2.0),
     TypeGame(3, R.string.nightmare, R.drawable.card_nigthmare,  R.string.nightmareDesc, 3.0),
     TypeGame(4, R.string.clothing,  R.drawable.card_clothing,   R.string.clothingDesc,  2.0),
-    TypeGame(5, R.string.history,   R.drawable.card_history,    R.string.historyDesc,   2.0),
+    TypeGame(5, R.string.history,   R.drawable.card_history,    R.string.historyDesc,  2.0),
     TypeGame(6, R.string.sport,     R.drawable.card_sport,      R.string.sportDesc,     2.0)
 )
